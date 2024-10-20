11d93f2f9a811a06b9fcd3f7adfe1f8670bf89e2
JeromeJinzh <jinzihan.jin@mail.utoronto.ca>
2024-10-21 03:48:04 +0800

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set a current user.
+     * @param name the name used for the username.
+     */
+    void setCurrentUser(String name);
 }
